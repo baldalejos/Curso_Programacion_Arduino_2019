7f9edb1ac2cb041243f69ddaa8e192ca38bb0882
baldalejos <baldalejos@gmail.com>
2019-09-09 18:31:28 +0200

    Codigo depurado

--- Ejercicio01-Comprueba_Entorno_Arduino/Ejercicio01-Comprueba_Entorno_Arduino.ino
+++ Ejercicio01-Comprueba_Entorno_Arduino/Ejercicio01-Comprueba_Entorno_Arduino.ino
@@ -3,9 +3,6 @@
 #define PIN_LDR A1
 LiquidCrystal_I2C lcd(0x3F, 16, 2); // set the LCD address to 0x27 or 0x3F, it depends, for a 16 chars and 2 line display
 void setup() {
-  //Inicializo puerto serie
-  Serial.begin(9600);
-   Serial.println(F("Inicializando pantalla..."));
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
